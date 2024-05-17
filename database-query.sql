2dfd457ad0e34ef42ecfd8e8f2ab3ceefcd37c96
Sadra Hosseini <101637120+Supersadra@users.noreply.github.com>
2024-05-17 16:46:43 +0330

    MS Access database deleted

--- database-query.sql
+++ database-query.sql
@@ -1,13 +1,13 @@
 -- INSERT INTO public.times (doctor_name,weekday,shift,hour,date,capacity,visit_count)
 -- VALUES ('مسعوده سبزه چیان','سه شنبه','عصر','16:00','2/25/1403',25,0)
 
--- INSERT INTO public.clinics (section,clinic)
--- VALUES ('فوق تخصص خون اطفال','کلینیک کودکان و اطفال')
+INSERT INTO public.clinics (section,clinic)
+VALUES ('فوق تخصص خون اطفال','کلینیک کودکان و اطفال')
 
 -- INSERT INTO public.doctors (name,code,section,expertise,shifts)
 -- VALUES ('مسعوده سبزه چیان',68834,'فوق تخصص کلیه اطفال','فوق تخصص کلیه اطفال','صبح،عصر')
 
-SELECT * FROM public.times
+-- SELECT * FROM public.times
 
 -- DELETE FROM public.visits
 
